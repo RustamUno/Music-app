6423ccec113ccccf9fbf1c5c4a7fd0d47850eb0d
RustamUno <gimaevr156@gmail.com>
2022-12-22 22:20:30 +0300

    Added logic

--- src/components/Main/CenterBlock/Filter/Filter.jsx
+++ src/components/Main/CenterBlock/Filter/Filter.jsx
@@ -1,40 +1,108 @@
-import '../../../../App.css'
+import {useState} from 'react';
+
 import * as S from './style'
-// import {useState} from 'react';
 
 // const author =[
-//     'Nero',
-//     'Dynoro, Outwork, Mr. Gee',
-//     'Ali Bakgor',
-//     'Стоункат, Psychopath',
-//     'Jaded, Will Clarke, AR/CO',
-//     'Blue Foundation, Zeds Dead',
-//     'HYBIT, Mr. Black, Offer Nissim, Hi Profile',
-//     'Minthaze',
-//     'Calvin Harris, Disciples',
-//     'Tom Boxer',
-//     'Frank Sinatra'
+    // 'Nero',
+    // 'Dynoro, Outwork, Mr. Gee',
+    // 'Ali Bakgor',
+    // 'Стоункат, Psychopath',
+    // 'Jaded, Will Clarke, AR/CO',
+    // 'Blue Foundation, Zeds Dead',
+    // 'HYBIT, Mr. Black, Offer Nissim, Hi Profile',
+    // 'Minthaze',
+    // 'Calvin Harris, Disciples',
+    // 'Tom Boxer',
+    // 'Frank Sinatra'
 // ]
 
 // const genre = [
-//     'Джаз',
-//     'Рок',
-//     'Хип-хоп',
-//     'Реп',
-//     'R&B',
-//     'Поп',
+    // 'Джаз',
+    // 'Рок',
+    // 'Хип-хоп',
+    // 'Реп',
+    // 'R&B',
+    // 'Поп',
 // ]
 
 function Filter(){
-    // const [visible, setVisible] = useState(true);
-    // const toggleVisibility =()=> setVisible(!visible);
+    
+    // const closeOpenFilter = () => {
+    //     setVisibleAuthor(false)
+    //     setVisibleYear(false)
+    //     setVisibleGenre(false)
+    // }
+
+    const [visibleAuthor, setVisibleAuthor] = useState(false);
+    const [visibleYear, setVisibleYear] = useState(false);
+    const [visibleGenre, setVisibleGenre] = useState(false);
+
+    const toggleVisibilityAuthor =()=> 
+    // closeOpenFilter()
+    setVisibleAuthor(!visibleAuthor);
+
+    const toggleVisibilityYear =()=> 
+    // closeOpenFilter()
+    setVisibleYear(!visibleYear);
+
+    const toggleVisibilityGenre =()=> 
+    // closeOpenFilter()
+    setVisibleGenre(!visibleGenre);
+
+
     return(
         <S.Filter>
             <S.Title>Искать по:</S.Title>
-            <div className="filter__button button-author _btn-text" >исполнителю</div>
+            <S.Btn onClick={toggleVisibilityAuthor} role="presentation">исполнителю{visibleAuthor} {visibleAuthor && (
+                <S.Open >
+                        <S.List>Nero</S.List>
+                        <S.List>Dynoro, Outwork, Mr. Gee</S.List>
+                        <S.List>Ali Bakgor</S.List>
+                        <S.List>Стоункат, Psychopath</S.List>
+                        <S.List>Jaded, Will Clarke, AR/CO</S.List>
+                        <S.List>Blue Foundation, Zeds Dead</S.List>
+                        <S.List>HYBIT, Mr. Black, Offer Nissim, Hi Profile</S.List>
+                        <S.List>Minthaze</S.List>
+                        <S.List>Calvin Harris, Disciples</S.List>
+                        <S.List>Tom Boxer</S.List>
+                        <S.List>Frank Sinatra</S.List>
+                </S.Open >     
+            )}</S.Btn>
+            
+            <S.Btn onClick={toggleVisibilityYear} role="presentation">году выпуска{visibleYear}{visibleYear && (
+                <S.Open >
+                    <S.Annum>
+                    <input
+                    type="radio"
+                    id="new"
+                    name="year"
+                    value="new"/>
+                    <S.Label  htmlFor="new">Новое</S.Label>
+                    </S.Annum>
 
-            <div className="filter__button button-year _btn-text">году выпуска</div>
-            <div className="filter__button button-genre _btn-text">жанру</div>
+                    <S.Annum>
+                    <input
+                    type="radio"
+                    id="old"
+                    name="year"
+                    value="old"/>
+                    <S.Label  htmlFor="old">Старое</S.Label>
+                    </S.Annum>
+
+                </S.Open >     
+            )}</S.Btn>
+            
+            <S.Btn onClick={toggleVisibilityGenre} role="presentation">жанру{visibleGenre}{visibleGenre && (
+                <S.Open >
+                    <S.List>Джаз</S.List>
+                    <S.List>Рок</S.List>
+                    <S.List>Хип-хоп</S.List>
+                    <S.List>Реп</S.List>
+                    <S.List> R&B</S.List>
+                    <S.List>Поп</S.List>
+                </S.Open >     
+            )}</S.Btn>
+
         </S.Filter>
         
     )
--- src/components/Main/CenterBlock/Filter/style.jsx
+++ src/components/Main/CenterBlock/Filter/style.jsx
@@ -1,17 +1,11 @@
 import styled from 'styled-components'
 
 export const Filter = styled.div`
-display: -webkit-box;
-display: -ms-flexbox;
 display: flex;
--webkit-box-orient: horizontal;
--webkit-box-direction: normal;
-    -ms-flex-direction: row;
-        flex-direction: row;
--webkit-box-align: center;
-    -ms-flex-align: center;
-        align-items: center;
+flex-direction: row;
+align-items: center;
 margin-bottom: 51px;
+gap: 10px;
 `
 export const Title = styled.div`
 font-style: normal;
@@ -19,4 +13,66 @@ font-weight: 400;
 font-size: 16px;
 line-height: 24px;
 margin-right: 15px;
+`
+
+export const Btn = styled.div`
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+border: 1px solid #FFFFFF;
+border-radius: 60px;
+padding: 6px 20px;
+
+:hover {
+    cursor: pointer;
+  }
+:active {
+    border-color: #AD61FF;
+    color: #AD61FF;
+    cursor: pointer;
+  }
+`
+
+export const Open= styled.div`
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 24px;
+color: #AD61FF;
+position: absolute; 
+padding: 34px;
+gap: 10px;
+background: #313131;
+border-radius: 12px; 
+`
+export const List=styled.a`
+display:flex;
+font-weight: 400;
+font-size: 20px;
+line-height: 24px;
+color: #FFFFFF;
+:hover{
+    color: #D9B6FF;
+}
+`
+
+export const Annum = styled.div`
+font-weight: 400;
+font-size: 20px;
+line-height: 24px;
+color: #FFFFFF;
+:hover{
+    color: #D9B6FF;
+`
+
+export const Label = styled.label`
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 24px;
+color: #FFFFFF;
+:hover{
+    color: #D9B6FF;
+}
 `
